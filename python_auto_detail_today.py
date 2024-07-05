26acb4e5fa9866f8d3d772afb9b738701ac33a4d
rifqanzalbina <zalbinarifqan19@gmail.com>
2024-07-05 18:44:02 +0700

    Update

--- python_auto_detail_today.py
+++ python_auto_detail_today.py
@@ -11,12 +11,5 @@ for i in range(0, 150):
         file.write(d + '\n')
     os.system('git add test.txt')
     os.system('git commit --date="2024-07-05" -m 1')
-    
-# for i in range(0, 150):
-#     d = str(i) + ' days ago'
-#     with open('test.txt', 'a') as file:
-#         file.write(d + '\n')
-#     os.system('git add test.txt')
-#     os.system('git commit --date="2024-07-05 -m 1')
 
 os.system('git push')  # Mengirim perubahan lokal ke remote repository
