5d8d98233754bab4132b5f781c8b9d60bb6c4861
rifqanzalbina <zalbinarifqan19@gmail.com>
2024-07-05 18:33:52 +0700

    Update

--- python_auto_detail_today.py
+++ python_auto_detail_today.py
@@ -1,23 +1,22 @@
-import os
-from datetime import datetime, timedelta
+import os, random
+
+# Mengatur upstream branch
+os.system('git branch --set-upstream-to=origin/main main')
 
 os.system('git pull')  # Mengambil perubahan terbaru dari remote repository
 
-today = datetime.now().date()
-num_commits_per_loop = 10  # Jumlah commit per loop
+for i in range(0, 150):
+    d = str(i) + ' days ago'
+    with open('test.txt', 'a') as file:
+        file.write(d + '\n')
+    os.system('git add test.txt')
+    os.system('git commit --date="2024-07-05" -m 1')
+    
+# for i in range(0, 150):
+#     d = str(i) + ' days ago'
+#     with open('test.txt', 'a') as file:
+#         file.write(d + '\n')
+#     os.system('git add test.txt')
+#     os.system('git commit --date="2024-07-05 -m 1')
 
-for i in range(50):
-    for j in range(num_commits_per_loop):
-        date = today - timedelta(days=i)
-        d = date.strftime('%Y-%m-%d')
-        if date <= today:
-            with open('test.txt', 'a') as file:
-                file.write(d + '\n')
-            os.system(f'git add test.txt')
-            os.system(f'git commit --date="{d}" -m "Commit on {d}"')
-        else:
-            break
-    if date > today:
-        break
-
-os.system('git push -u origin main')
+os.system('git push')  # Mengirim perubahan lokal ke remote repository
